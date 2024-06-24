bdb4d3a109f5b62a9448b964d95cda052edb468b
krishnajith <krish@pop-os.localdomain>
2024-06-24 19:42:52 +0530

    first commit

---
+++ redis_client.py
@@ -0,0 +1,175 @@
+import redis
+import os
+import time
+import random
+import sys
+import uuid
+
+
+SESSION_ID = "transactionRedisDbOperations"
+
+
+def formatPassFunctionCmd(file_name, requirements):
+    req = "REQUIREMENTS"
+    cmd = (
+        f'sudo docker exec -it redis-gear redis-cli RG.PYEXECUTE  "`cat {file_name}`"  '
+    )
+    # Add ID & UPGRADE(to prevent duplicate register functions)
+    cmd += f"ID {SESSION_ID} UPGRADE "
+    for r in requirements:
+        req += f" {r}"
+    res = cmd + req
+    return res
+
+
+"""
+PassFunctionToRedis : Helper function to pass a python file to redis server
+GetAllRegistrations : Helps to dump all registered events
+UnRegister : To unregister a particular event
+RedisLogs : Live redis logs
+RedisClient : Redis cli
+"""
+
+CMDS = {
+    "PassFunctionToRedis": formatPassFunctionCmd,
+    "GetAllRegistrations": f"sudo docker exec -it redis-gear redis-cli RG.DUMPREGISTRATIONS",
+    "UnRegister": lambda registerId: f"sudo docker exec -it redis-gear redis-cli RG.UNREGISTER {registerId}",
+    "RedisLogs": f"sudo docker logs redis-gear -f",
+    "RedisClient": f"sudo docker exec -it redis-gear redis-cli",
+}
+
+
+def connect_to_redis():
+    try:
+        print("connecting to redis....")
+        r = redis.Redis(host="0.0.0.0", port=6379, decode_responses=True)
+        return r, False
+    except Exception as e:
+        print("An Error Occured....")
+        return str(e), True
+
+
+def set_dict(key, data_dict, r):
+    r.hset(key, mapping=data_dict)
+
+
+def set_json_value(key, data_dict, r):
+    r.json().set(key, "$", data_dict)
+
+
+def get_dict(key, r):
+    return r.hgetall(key)
+
+
+def list_keys(r, pattern):
+    return r.keys(pattern=pattern, decode_responses=True)
+
+
+def run_redis_cli_cmd(cmd):
+    os.system(cmd)
+
+
+def update_audio_played(key, r):
+    r.json().set(key, "$.audioPlayed", random.randint(1, 5))
+
+
+def main():
+    """ "
+    How to run this script ?
+    python3 redis_client.py arg
+    arg => 1 For passing redis_cron_job.py file to redis
+    arg => 2 For inserting data to redis
+    arg => 3 For running tests
+    """
+    r, err = connect_to_redis()
+    r.keys()
+    if err:
+        print(f"Error : {r}")
+    else:
+        print("Connected To Redis")
+
+    args = sys.argv
+
+    n = len(args)
+
+    if n < 2:
+        raise Exception("No Argument Found")
+
+    # Pass Python Functions To Redis Gear
+    if args[1] == "1":
+        print("--------------Passing Function To RedisGear To Execute------------")
+        get_cmd = CMDS["PassFunctionToRedis"](
+            "redis_gear_functions.py", ["pymongo", "python-dotenv"]
+        )
+        run_redis_cli_cmd(get_cmd)
+
+    # SET a single entry to redis
+    if args[1] == "2":
+        print("--------------Creating A New JSON data in Redis------------")
+        num = random.randint(1, 100000)
+        data = {
+            "reqRefNo": f"REQ123456789{num}",
+            "rrn": "RRN987654321",
+            "mid": "MERCHANT123",
+            "tid": "TERM456",
+            "mcc": "1234",
+            "meVpa": "merchant@bank",
+            "transactionType": 1 + num,
+            "transactionMode": 2,
+            "txnAmt": "100.50",
+            "txnTimestamp": "2024-06-22T12:34:56Z",
+            "timeStamp": int(time.time()),
+            "deviceId": 1234567890,
+            "expirationTime": 1624198696,
+            "tMsgRecvByServer": int(time.time()),
+            "tMsgRecvFromDev": 1624197701,
+            "audioPlayed": 1,
+            "id": 9876543210 + num,
+        }
+        # Create a unique key
+        unique_key = uuid.uuid4().hex
+        set_json_value(f"transaction:{unique_key}", data, r)
+
+    # TESTS
+    if args[1] == "3":
+        print("------------------Running Tests----------------------")
+        start = time.time()
+        cnt = 100
+        device_id_lst = []
+        while cnt > 0:
+            num = random.randint(1, 100000)
+            data = {
+                "reqRefNo": f"REQ123456789{num}",
+                "rrn": "RRN987654321",
+                "mid": "MERCHANT123",
+                "tid": "TERM456",
+                "mcc": "1234",
+                "meVpa": "merchant@bank",
+                "transactionType": 1 + num,
+                "transactionMode": 2,
+                "txnAmt": "100.50",
+                "txnTimestamp": "2024-06-22T12:34:56Z",
+                "timeStamp": int(time.time()),
+                "deviceId": 1234567890 + cnt,
+                "expirationTime": 1624198696,
+                "tMsgRecvByServer": int(time.time()),
+                "tMsgRecvFromDev": 1624197701,
+                "audioPlayed": random.randint(0, 5),
+                "id": 9876543210 + num,
+            }
+            device_id_lst.append(data["deviceId"])
+            # Create a unique key
+            unique_key = uuid.uuid4().hex
+            set_json_value(f"transaction:{unique_key}", data, r)
+            temp = cnt
+            if temp % 5 == 0 and data["audioPlayed"] <= 0:
+                # Update the `audioPlayed` field to a value greater than 0
+                update_audio_played(f"transaction:{unique_key}", r)
+            cnt -= 1
+        total_length = len(device_id_lst)
+        distinct_length = len(set(device_id_lst))
+        print("No of Duplcate Entries : ", total_length - distinct_length)
+        end = time.time()
+        print("diff :", end-start)
+if __name__ == "__main__":
+    main()
---
+++ redis_gear_functions.py
@@ -0,0 +1,216 @@
+import time
+import pymongo
+import json
+import os
+
+
+config = {
+    "JOB_INTERVAL":int(os.environ['JOB_INTERVAL']),
+    "PREFIX":os.environ['PREFIX'],
+    "MOVEMENT_TIME":int(os.environ['MOVEMENT_TIME']),
+    "MULTIPLIER":int(os.environ['MULTIPLIER']),
+    "CONN_STRING":os.environ['CONN_STRING'],
+    "COLLECTION_NAME":os.environ['COLLECTION_NAME'],
+    "DB_NAME":os.environ['DB_NAME'],
+    "RETRY_CONN_COUNT":int(os.environ['RETRY_CONN_COUNT']),
+
+}
+
+log(f'config : {config}')
+
+JOB_INTERVAL = config['JOB_INTERVAL']
+PREFIX = config['PREFIX']
+MOVEMENT_TIME = config['MOVEMENT_TIME']
+EXPIRY_TIME = MOVEMENT_TIME*config['MULTIPLIER']
+
+
+
+class DBConnect:
+    """
+
+    This class implements Singleton Pattern
+
+    """
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DBConnect, cls).__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self) -> None:
+        if self._initialized:
+            return
+        self.conn_string = config['CONN_STRING']
+        self.client = None
+        self.collection_name = config['COLLECTION_NAME']
+        self.db_name = config['DB_NAME']
+        self.RETRY_CONN_COUNT = config['RETRY_CONN_COUNT']
+        self._initialized = True
+
+    def connect_to_db(self) -> bool:
+        if self.client is None:
+            while self.RETRY_CONN_COUNT > 0:
+                try:
+                    self.client = pymongo.MongoClient(self.conn_string)
+                    db = self.client[self.db_name]
+                    self.collection = db[self.collection_name]
+                    log("Connected to DB")
+                    return True
+                except Exception as e:
+                    log(f"-----An Error Occurred During DB connection-----")
+                    log(f"Error : {str(e)}")
+                    log(f"Retrying connection after 5 seconds")
+                    time.sleep(5)
+                    self.RETRY_CONN_COUNT -= 1
+            log(f"-----DB connection failed-----")
+            return False
+        return True
+
+    @property
+    def get_db_client(self):
+        return self.client
+
+    @property
+    def get_collection_name(self):
+        return self.collection_name
+
+    @property
+    def get_database_name(self):
+        return self.db_name
+
+
+def store_expired_data(data=None):
+    if data:
+        dbConnObj = DBConnect()
+        res = dbConnObj.connect_to_db()
+        if not res:
+            log(
+                "-------DB connection error store_expired_data Function Not Registered Properly------"
+            )
+            return
+        log(
+            "-------------------Transaction For Inserting Expired Data to DB-------------------"
+        )
+
+        # Get all keys which belongs to transaction
+        keys = execute("keys", f"{PREFIX}:*")
+        # Get current time
+        epoch_time_now = int(time.time())
+        # Get each key in key set
+        for key in keys:
+            # Convert string to dict
+            get_value = json.loads(execute("json.get", key))
+            # Get Creation time
+            tmsg_recvby_server = get_value.get("tMsgRecvByServer", None)
+            device_id = get_value.get("deviceId", None)
+            if tmsg_recvby_server is None or not isinstance(tmsg_recvby_server, int):
+                log(
+                    f"tMsgRecvByServer Key Not Found or tMsgRecvByServer is Not an Integer For Key {key}!"
+                )
+                continue
+
+            if device_id  is None:
+                log(f"DeviceId is Missing For Key {key}")
+                continue
+
+            exipiry_time_key = tmsg_recvby_server + MOVEMENT_TIME
+            # Compare whether the key is expired
+            if exipiry_time_key < epoch_time_now:
+                log(f"------- Key {key} Expired -------")
+                client = dbConnObj.get_db_client
+                db = client[dbConnObj.get_database_name]
+                collection = db[dbConnObj.get_collection_name]
+                if (
+                    collection.count_documents(
+                        {"tMsgRecvByServer": tmsg_recvby_server,"DeviceId":device_id}, limit=1
+                    )
+                    != 0
+                ):
+                    log("----------Document Already Exist With This tMsgRecvByServer----------")
+                else:
+                    log(f"------Inserting data : key {key}------")
+                    collection.insert_one(get_value)
+                # Delete the key from Redis
+                execute("json.del", key)
+                log("Data inserted into MongoDB and deleting from Redis")
+
+    # Reset the job key with the JOB_INTERVAL
+    execute("set", "jobkey{%s}" % hashtag(), "val", "EX", str(JOB_INTERVAL))
+
+
+def write_updates_to_db(data):
+    dbConnObj = DBConnect()
+    res = dbConnObj.connect_to_db()
+    if not res:
+        log(
+            "-------DB connection error write_updates_to_db Function Not Registered Properly------"
+        )
+        return
+    log(
+        "-------------------Transaction For Inserting Updated Data to DB-------------------"
+    )
+    
+    key = data.get("key")
+    get_value = json.loads(execute("json.get", key))
+    is_audio_played = get_value.get("audioPlayed", None)
+    tmsg_recvby_server = get_value.get("tMsgRecvByServer", None)
+    device_id = get_value.get("deviceId", None)
+    
+    if is_audio_played is None or not isinstance(is_audio_played, int):
+        log("audioPlayed Key Not Found or audioPlayed is Not an Integer!")
+    elif tmsg_recvby_server is None or not isinstance(tmsg_recvby_server, int):
+        log("tMsgRecvByServer Key Not Found or tMsgRecvByServer is Not an Integer!")
+    elif is_audio_played > 0:
+        log(f"------audioPlayed is updated for key {key}------")
+        client = dbConnObj.get_db_client
+        db = client[dbConnObj.get_database_name]
+        collection = db[dbConnObj.get_collection_name]
+        if (
+            collection.count_documents(
+               {"tMsgRecvByServer": tmsg_recvby_server,"DeviceId":device_id}, limit=1
+            )
+            != 0
+        ):
+            log("----------Document Already Exist With This tMsgRecvByServer")
+        else:
+            log(f"------Inserting data : key {key}------")
+            collection.insert_one(get_value)
+        # Delete the key from Redis
+        execute("json.del", key)
+        log("Data inserted into MongoDB and deleting from Redis")
+
+
+# Set Expiry On Every Key With Prefix transaction
+# This is a default expiry key which will always be greater than the actual expiry time
+gbExpiry = GB()
+
+gbExpiry.foreach(lambda x: execute("EXPIRE", x["key"], str(EXPIRY_TIME)))
+gbExpiry.foreach(lambda x: log(f"TTL For Key {x['key']} {execute('TTL', x['key'])}"))
+gbExpiry.register(f"{PREFIX}:*", mode="sync", readValue=False)
+
+# Register the RedisGears function
+gbCron = GB()
+# This event will trigger on each JOB INTERVAL, Hence acting as a cron function
+gbCron.foreach(lambda x: log("Setting Up the Cron Function")).foreach(
+    lambda x: store_expired_data(x)
+).register(
+    prefix="jobkey*",
+    eventTypes=["expired"],
+    readValue=False,
+    mode="sync",
+    onRegistered=store_expired_data,
+)
+
+gbUpdate = GB()
+# This event will trigger on set event on transaction data
+gbUpdate.foreach(
+    lambda x: log("Setting Up the Write & Update Event Listening Functions")
+).foreach(lambda x: write_updates_to_db(x)).register(
+    prefix=f"{PREFIX}:*",
+    eventTypes=["json.set"],
+    readValue=False,
+    mode="sync",
+)
